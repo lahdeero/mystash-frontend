2a920e204c3760472aed107313da2a0f0f6efee6
Eero Lahdenperä <eero.lahdenpera@gmail.com>
2019-04-28 01:51:54 +0300

    added PUBLIC_URL to note routes and createNote doesnt change previous state/store anymore

--- build/precache-manifest.c499a7f6260552b6406a5f9046d77a3a.js
+++ build/precache-manifest.c499a7f6260552b6406a5f9046d77a3a.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/mystash-frontend/static/js/runtime~main.4f84a99e.js"
   },
   {
-    "revision": "86cfc0a0670da3753a77",
-    "url": "/mystash-frontend/static/js/main.86cfc0a0.chunk.js"
+    "revision": "c0ff549cd89832521a91",
+    "url": "/mystash-frontend/static/js/main.c0ff549c.chunk.js"
   },
   {
     "revision": "32c36bf13ff074a5d5a4",
     "url": "/mystash-frontend/static/js/2.32c36bf1.chunk.js"
   },
   {
-    "revision": "86cfc0a0670da3753a77",
+    "revision": "c0ff549cd89832521a91",
     "url": "/mystash-frontend/static/css/main.0bfc2b1f.chunk.css"
   },
   {
-    "revision": "c9da035d42ecbb93fa589b98eb3c1a6f",
+    "revision": "a83006613e4a14647efe7007cb88aec2",
     "url": "/mystash-frontend/index.html"
   }
 ];
