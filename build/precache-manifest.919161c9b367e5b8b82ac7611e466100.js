24f4cb79c7a1870d527cb53f9a47462882088fbd
Eero Lahdenperä <eero.lahdenpera@gmail.com>
2019-04-28 01:19:49 +0300

    removed build from gitignore and added 404.html

---
+++ build/precache-manifest.919161c9b367e5b8b82ac7611e466100.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f84a99ee6ef8222e916",
+    "url": "/mystash-frontend/static/js/runtime~main.4f84a99e.js"
+  },
+  {
+    "revision": "adf69afd7c6698081a42",
+    "url": "/mystash-frontend/static/js/main.adf69afd.chunk.js"
+  },
+  {
+    "revision": "32c36bf13ff074a5d5a4",
+    "url": "/mystash-frontend/static/js/2.32c36bf1.chunk.js"
+  },
+  {
+    "revision": "adf69afd7c6698081a42",
+    "url": "/mystash-frontend/static/css/main.0bfc2b1f.chunk.css"
+  },
+  {
+    "revision": "11e9efb60b140e0524ba70466bd572ec",
+    "url": "/mystash-frontend/index.html"
+  }
+];
