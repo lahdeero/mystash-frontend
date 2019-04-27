78e292e74a73e32f4e02bc6f89fd851663691b97
Eero Lahdenperä <eero.lahdenpera@gmail.com>
2019-04-27 23:35:31 +0300

    Updates

---
+++ precache-manifest.d967286f5b8cccb26bbe12082303da93.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f84a99ee6ef8222e916",
+    "url": "/mystash-frontend/static/js/runtime~main.4f84a99e.js"
+  },
+  {
+    "revision": "9edf956cab85e2c3d392",
+    "url": "/mystash-frontend/static/js/main.9edf956c.chunk.js"
+  },
+  {
+    "revision": "2e6cb3009626041a3f12",
+    "url": "/mystash-frontend/static/js/2.2e6cb300.chunk.js"
+  },
+  {
+    "revision": "9edf956cab85e2c3d392",
+    "url": "/mystash-frontend/static/css/main.0bfc2b1f.chunk.css"
+  },
+  {
+    "revision": "6cae569d14eabb9904986cd7422879a3",
+    "url": "/mystash-frontend/index.html"
+  }
+];
