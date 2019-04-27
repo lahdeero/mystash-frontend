46e7b723b078725d01822d71e98c1dea4afef53a
Eero Lahdenperä <eero.lahdenpera@gmail.com>
2019-04-27 23:38:36 +0300

    build to .gitignore

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.02acfc343cc7fc10c917539fc2b81c7b.js"
+  "/mystash-frontend/precache-manifest.d967286f5b8cccb26bbe12082303da93.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/mystash-frontend/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
