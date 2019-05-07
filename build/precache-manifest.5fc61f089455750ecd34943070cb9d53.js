dcadf1aae369704d64e09dc5999b003c184589b8
Eero Lahdenperä <eero.lahdenpera@gmail.com>
2019-05-07 03:02:36 +0300

    attempt to fix routing at github pages

--- build/precache-manifest.5fc61f089455750ecd34943070cb9d53.js
+++ build/precache-manifest.5fc61f089455750ecd34943070cb9d53.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "c9b0425f1637d45d53b6",
+    "revision": "f526273d27a37e294785",
     "url": "/mystash-frontend/static/css/main.0ba2f37c.chunk.css"
   },
   {
-    "revision": "c9b0425f1637d45d53b6",
-    "url": "/mystash-frontend/static/js/main.c9b0425f.chunk.js"
+    "revision": "f526273d27a37e294785",
+    "url": "/mystash-frontend/static/js/main.f526273d.chunk.js"
   },
   {
     "revision": "4f84a99ee6ef8222e916",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/mystash-frontend/static/js/2.e8a78698.chunk.js"
   },
   {
-    "revision": "8ff063cd57c324a9463164ba202e79e1",
+    "revision": "aad2ed3d6beba27df856f5ed0496ea42",
     "url": "/mystash-frontend/index.html"
   }
 ];
