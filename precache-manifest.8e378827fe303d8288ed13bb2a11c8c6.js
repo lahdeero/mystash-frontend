7113e27c8afb68201dbbe234aa36efa70079cd12
Eero Lahdenperä <eero.lahdenpera@gmail.com>
2019-09-01 03:58:38 +0300

    Updates

--- precache-manifest.8e378827fe303d8288ed13bb2a11c8c6.js
+++ precache-manifest.8e378827fe303d8288ed13bb2a11c8c6.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/mystash-frontend/static/js/runtime~main.4f84a99e.js"
   },
   {
-    "revision": "40ced056d561b6d9f95e",
-    "url": "/mystash-frontend/static/js/main.40ced056.chunk.js"
+    "revision": "2e2a1da3effa490be770",
+    "url": "/mystash-frontend/static/js/main.2e2a1da3.chunk.js"
   },
   {
     "revision": "66e28df4b9d2086eb152",
     "url": "/mystash-frontend/static/js/2.66e28df4.chunk.js"
   },
   {
-    "revision": "40ced056d561b6d9f95e",
+    "revision": "2e2a1da3effa490be770",
     "url": "/mystash-frontend/static/css/main.0bfc2b1f.chunk.css"
   },
   {
-    "revision": "41514b2ae7c8137075a806303c79a910",
+    "revision": "0c88c679a4af302c866794918904712b",
     "url": "/mystash-frontend/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/mystash-frontend/precache-manifest.663de8f60a90736088798b1c3f0d04c9.js"
+  "/mystash-frontend/precache-manifest.8e378827fe303d8288ed13bb2a11c8c6.js"
 );
 
 workbox.clientsClaim();
