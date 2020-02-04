8ddcbc10a8a5856ee6a58e8deb78dff2f899372b
Eero Lahdenperä <eero.lahdenpera@gmail.com>
2020-02-04 04:10:08 +0200

    backend url fixed

--- src/utils/environmentResolvers.js
+++ src/utils/environmentResolvers.js
@@ -1,7 +1,7 @@
 export function resolveUrl() {
   const host = window.location.host
   console.log('host = ', host)
-  const ec2 = 'https://ec2-13-48-47-185.eu-north-1.compute.amazonaws.com'
+  const cloudfront = 'https://d3eipmu8grncj0.cloudfront.net'
   // const heroku = 'https://my-stash.herokuapp.com'
-  return host === 'localhost:3000' ? 'http://Localhost:3001' : ec2
+  return host === 'localhost:3000' ? 'http://Localhost:3001' : cloudfront
 }
