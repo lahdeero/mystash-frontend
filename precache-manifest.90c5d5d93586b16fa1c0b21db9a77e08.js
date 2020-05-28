dcee12f8f8c2e1f7e5767da8759c4b1c5124f921
Eero Lahdenperä <eero.lahdenpera@gmail.com>
2020-05-28 15:33:57 +0300

    Updates

---
+++ precache-manifest.90c5d5d93586b16fa1c0b21db9a77e08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9997f3116d294517e95e",
+    "url": "/mystash-frontend/static/css/main.df976603.chunk.css"
+  },
+  {
+    "revision": "9997f3116d294517e95e",
+    "url": "/mystash-frontend/static/js/main.9997f311.chunk.js"
+  },
+  {
+    "revision": "d0874d9b25e44e94bf09",
+    "url": "/mystash-frontend/static/js/runtime~main.d0874d9b.js"
+  },
+  {
+    "revision": "a4e8ea46753c9a90cec7",
+    "url": "/mystash-frontend/static/js/2.a4e8ea46.chunk.js"
+  },
+  {
+    "revision": "8883c03778f3f82d1fb3a689ade1e3ed",
+    "url": "/mystash-frontend/static/js/2.a4e8ea46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0193b8bd102a20a6d72e6d555a92daa8",
+    "url": "/mystash-frontend/index.html"
+  }
+];
