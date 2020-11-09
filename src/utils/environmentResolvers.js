28de57f8659fe73dc003ca398cdb1d786bae5a00
Eero Lahdenperä <eero.lahdenpera@gmail.com>
2020-11-09 19:33:04 +0200

    New backend url

--- src/utils/environmentResolvers.js
+++ src/utils/environmentResolvers.js
@@ -1,7 +1,8 @@
 export function resolveUrl() {
   const host = window.location.host
   console.log('host = ', host)
-  const cloudfront = 'https://d3eipmu8grncj0.cloudfront.net'
+  // const cloudfront = 'https://d3eipmu8grncj0.cloudfront.net'
   // const heroku = 'https://my-stash.herokuapp.com'
-  return host === 'localhost:3000' ? 'http://Localhost:8080' : cloudfront
+  const caligula = 'https://caligula.duckdns.org:8080'
+  return host === 'localhost:3000' ? 'http://Localhost:8080' : caligula
 }
