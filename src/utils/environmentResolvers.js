4f23cc20a1e0b95d14f64903c9ddde01fa00a8a2
Eero Lahdenperä <eero.lahdenpera@gmail.com>
2021-07-17 04:36:22 +0300

    Backend moved to DO

--- src/utils/environmentResolvers.js
+++ src/utils/environmentResolvers.js
@@ -3,6 +3,7 @@ export function resolveUrl() {
   console.log('host = ', host)
   // const cloudfront = 'https://d3eipmu8grncj0.cloudfront.net'
   // const heroku = 'https://my-stash.herokuapp.com'
-  const caligula = 'https://caligula.duckdns.org:8080'
-  return host === 'localhost:3000' ? 'http://Localhost:8080' : caligula
+  // const caligula = 'https://caligula.duckdns.org:8080'
+  const droplet = 'https://mystash.duckdns.org:8080'
+  return host === 'localhost:3000' ? 'http://Localhost:8080' : droplet
 }
